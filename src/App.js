cacffec0e8213275ca6988d94d2b5b401420b03c
zeke <zkemmerling@gmail.com>
2024-05-31 18:55:13 -0400

    Spotify React project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,168 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React, { useState, useCallback } from "react";
+import "./App.css";
+
+import Spotify from "./Spotify.js";
 
 function App() {
+  const [searchResults, setSearchResults] = useState([]);
+  const [playlistTracks, setPlaylistTracks] = useState([]);
+  const [playlistName, setPlaylistName] = useState("New Playlist");
+
+  const search = useCallback((term) => {
+    Spotify.search(term).then(setSearchResults);
+  }, []);
+
+  const updateName = useCallback((name) => {
+    setPlaylistName(name);
+  }, []);
+
+  const deleteTrack = useCallback((track) => {
+    setPlaylistTracks((prev) =>
+      prev.filter((currTrack) => currTrack.id !== track.id),
+    );
+  }, []);
+
+  const addTrack = useCallback(
+    (track) => {
+      setPlaylistTracks((prev) => [...prev, track]);
+    },
+    [playlistTracks],
+  );
+
+  const savePlaylist = useCallback(() => {
+    const trackUris = playlistTracks.map((track) => track.uri);
+    Spotify.savePlaylist(playlistName, trackUris).then(() => {
+      setPlaylistName("New Playlist");
+      setPlaylistTracks([]);
+    });
+  }, [playlistName, playlistTracks]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Jammmy</h1>
+      <div className="Search">
+        <SearchBar onSearch={search} />
+      </div>
+      <div>
+        <SearchResults searchResults={searchResults} addTrack={addTrack} />
+        <Playlist
+          playlistName={playlistName}
+          playlistTracks={playlistTracks}
+          savePlaylist={savePlaylist}
+          updateName={updateName}
+          deleteTrack={deleteTrack}
+        />
+      </div>
+    </div>
+  );
+}
+
+const SearchBar = (props) => {
+  // stores the value of our query in react state of the component
+  const [term, setTerm] = useState("");
+  // on updating the input field (adding/changing the query), the query stored in state must be updated
+
+  const handleTermChange = useCallback((event) => {
+    setTerm(event.target.value);
+  }, []);
+
+  // execute a search with the Spotify search method passed thru props
+  const search = useCallback(() => {
+    props.onSearch(term);
+  }, [props.onSearch, term]);
+
+  return (
+    <div className="SearchBar">
+      <input
+        type="text"
+        placeholder="Search Jammy"
+        onChange={handleTermChange}
+      />
+      <button className="SearchButton" onClick={search}>
+        Search
+      </button>
+    </div>
+  );
+};
+
+function SearchResults(props) {
+  return (
+    <div>
+      <h2>Results</h2>
+      <Tracklist tracks={props.searchResults} onAdd={props.addTrack} />
+    </div>
+  );
+}
+
+function Playlist(props) {
+  const handleNameChange = useCallback(
+    (event) => {
+      props.updateName(event.target.value);
+    },
+    [props.updateName],
+  );
+
+  return (
+    <div>
+      <input
+        type="text"
+        defaultValue="New Playlist"
+        onChange={handleNameChange}
+      />
+      <Tracklist
+        tracks={props.playlistTracks}
+        onRemove={props.deleteTrack}
+        isRemoval={true}
+      />
+      <button onClick={props.savePlaylist}>Save</button>
+    </div>
+  );
+}
+
+function Tracklist(props) {
+  if (props.tracks) {
+    return (
+      <div>
+        {props.tracks.map((track) => {
+          return (
+            <Track
+              track={track}
+              key={track.id}
+              onAdd={props.onAdd}
+              onRemove={props.onRemove}
+              isRemoval={props.isRemoval}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+function Track(props) {
+  const addTrack = useCallback(() => {
+    props.onAdd(props.track);
+  }, [props.onAdd, props.track]);
+
+  const removeTrack = useCallback(() => {
+    props.onRemove(props.track);
+  }, [props.onRemove, props.track]);
+
+  const renderButton = () => {
+    if (props.isRemoval) {
+      return <button onClick={removeTrack}>-</button>;
+    }
+    return <button onClick={addTrack}>+</button>;
+  };
+
+  return (
+    <div>
+      <h4>{props.track.song}</h4>
+      <p>
+        {props.track.artist} | {props.track.album}
+      </p>
+      {renderButton()}
     </div>
   );
 }
---
+++ src/Spotify.js
@@ -0,0 +1,89 @@
+require("dotenv").config();
+console.log(process.env.CLIENT_ID);
+
+let accessToken;
+
+const Spotify = {
+  getToken() {
+    const clientId = process.env.CLIENT_ID;
+    const redirectUri = process.env.REDIRECT_URI;
+
+    if (accessToken) {
+      return accessToken;
+    }
+
+    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+    if (accessTokenMatch && expiresInMatch) {
+      accessToken = accessTokenMatch[1];
+      const expiresIn = Number(expiresInMatch[1]);
+      window.setTimeout(() => (accessToken = ""), expiresIn * 1000);
+      window.history.pushState("Access Token", null, "/"); // This clears the parameters, allowing us to grab a new access token when it expires.
+      return accessToken;
+    } else {
+      const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+      window.location = accessUrl;
+    }
+  },
+
+  // query spotify for a song
+  search(term) {
+    const accessToken = Spotify.getToken();
+    return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((jsonResponse) => {
+        if (!jsonResponse.tracks) {
+          return [];
+        }
+        return jsonResponse.tracks.items.map((track) => ({
+          id: track.id,
+          song: track.name,
+          artist: track.artists[0].name,
+          album: track.album.name,
+          uri: track.uri,
+        }));
+      });
+  },
+
+  savePlaylist(playlistName, trackUris) {
+    if (!playlistName || !trackUris.length) {
+      console.log(playlistName, trackUris);
+      return;
+    }
+
+    const accessToken = Spotify.getToken();
+    const headers = { Authorization: `Bearer ${accessToken}` };
+
+    // fetch our user id -- required to access spotify playlist data for user
+    return fetch("https://api.spotify.com/v1/me", { headers: headers })
+      .then((r) => r.json())
+      .then((user_r) =>
+        // create new playlist for given user id
+        fetch(`https://api.spotify.com/v1/users/${user_r.id}/playlists`, {
+          method: "POST",
+          headers: headers,
+          body: JSON.stringify({ name: playlistName }),
+        })
+          .then((r) => r.json())
+          .then((r) =>
+            // add tracks to new playlist with given playlist id
+            fetch(
+              `https://api.spotify.com/v1/users/${user_r.id}/playlists/${r.id}/tracks`,
+              {
+                method: "POST",
+                headers: headers,
+                body: JSON.stringify({ uris: trackUris }),
+              },
+            ).then((r) => console.log(r.status)),
+          ),
+      );
+  },
+};
+
+export default Spotify;
